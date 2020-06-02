7636e6b309d6d2da96ed9f667d52d617a3cddcc5
Greg Leitheiser <greg@servantscode.org>
2020-06-02 09:37:19 -0500

    Formation work and start in on parishioner facing features

--- pm-config/kube-config.sh
+++ pm-config/kube-config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo Creating kubernetes configmaps
+echo Creating configmaps
 kubectl create configmap logstash-config --from-file logstash/config/logstash.yml
 kubectl create configmap pipeline-config --from-file logstash/pipeline/logstash.conf
+
+kubectl create configmap app-config --from-file config/env.js
 echo Configmap creation complete
+
